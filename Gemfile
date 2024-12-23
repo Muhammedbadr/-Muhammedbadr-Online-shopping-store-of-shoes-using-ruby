49b0f8819a0bcb69f9beb0a39151788734d5e989
muhammad <mukhammadnur6685@gamil.com>
2024-12-23 17:07:05 +0300

    first commit

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ group :test do
 end
 
 gem "devise"
+gem "devise-i18n"
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module RailsEcommerce
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale= :ar
   end
 end
