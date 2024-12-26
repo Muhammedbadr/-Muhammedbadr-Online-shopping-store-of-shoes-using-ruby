9e5f8a12a326e62a25ac64a85434528efb8305ea
Muhammedbedir <mukhammadnur6685@gmail.com>
2024-12-26 11:50:25 +0300

    some edit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController 
     def index 
+        @products = Product.last(6)
     end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+5.times do |index|
+    Category.create!(
+        title: "file #{index}"
+    )
+end
+Product.destroy_all
+20.times do |index|
+  Product.create!(
+    name: "product #{index}",
+    details: "details #{index}",
+    price: index * 50,
+    image_url: "0#{1 + index % 8}.jpg", # Removed "products/" prefix here
+    size: 1 + index % 3,
+    category_id: 1 + index % 5
+  )
+end
